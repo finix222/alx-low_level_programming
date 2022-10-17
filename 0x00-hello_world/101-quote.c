241ca58f6304194f477c472a8b26c54d69e044d6
ifeanyi oshilike <ifeanyioshaway@gmail.com>
2022-10-17 11:37:53 -0700

    m

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#include <string.h>
+
+#include <stdlib.h>
+
+#include <unistd.h>
+
+/*
+ * main - starting point of all C programs
+ *
+ * Return: returns an integer
+ */
+
+	int main(void)
+
+{
+
+		char q[30] = "- Dora Korpar, 2015-10-19\n";
+
+		fprintf(stderr, "and that piece of art is useful\" %s", q);
+return (1);
+}
