ef30e0dbba05a8c779ab6e9f7ee0898b6f696019
ifeanyi oshilike <ifeanyioshaway@gmail.com>
2022-10-17 13:34:47 -0700

    add

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,23 +1,20 @@
 #include <stdio.h>
-
 #include <string.h>
-
 #include <stdlib.h>
-
 #include <unistd.h>
-
-/*
- * main - starting point of all C programs
+/**
+ * main - Entry point
+ * Description: 'the program's description
+ * Return: Always 0 (Success)
  *
- * Return: returns an integer
  */
-
 	int main(void)
-
 {
 
 		char q[30] = "- Dora Korpar, 2015-10-19\n";
+		/* nawa */
 
 		fprintf(stderr, "and that piece of art is useful\" %s", q);
-return (1);
+	return (1);
 }
+/* returns value */
