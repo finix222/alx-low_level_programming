40fe40e4e1346d93c9615ad986ccf9cc2c7cd626
ifeanyi oshilike <ifeanyioshaway@gmail.com>
2022-10-18 18:42:04 -0700

    y

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,31 +1,17 @@
 #include <stdio.h>
-
-#include "_putchar.c"
-
+#include "main.h"
 /**
- * print_alphabet - prints all lowercase letters
- * Return - returns nothing
- */
-
+* print_alphabet - prints all lowercase letters
+* Return - returns nothing
+*/
 void print_alphabet(void)
-
 {
-
-		char ch = 'a';
+	char ch = 'a';
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-	_putchar(ch);
-	if (ch == 'z')
-	_putchar('\n');
+		_putchar(ch);
+		if (ch == 'z')
+			_putchar('\n');
 	}
-}
-/**
- * main - entry point of all C programms
- * Return: returns an integer
- */
-int main(void)
-	{
-	print_alphabet();
-return (0);
 }
