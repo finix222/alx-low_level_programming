602315eb1c42288225c6e9a992675d9b1a247616
ifeanyi oshilike <ifeanyioshaway@gmail.com>
2022-10-18 18:53:00 -0700

    oshilike

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* add - for returning absolut value of a number re
+* @a: number to be absoluted
+* @b: second number to be added
+* Return: absoluted integert
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
